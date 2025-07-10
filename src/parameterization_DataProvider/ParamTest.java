8e60997cc949e57dc5ad752a338d5d66a80cf08c
Payal <P2Test93>
2025-07-10 19:06:23 +0530

    Parallel Testing

--- src/parameterization_DataProvider/ParamTest.java
+++ src/parameterization_DataProvider/ParamTest.java
@@ -5,9 +5,11 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class ParamTest {
@@ -15,14 +17,27 @@ public class ParamTest {
 	WebDriver driver;
 	
 	@BeforeClass
-	void setup()
+	@Parameters({"browser"})
+	void setup(String nameofbrowser) throws InterruptedException
 	{
-		// Set up WebDriver
-        System.setProperty("webdriver.chrome.driver", "E:\\chromedriver_win32\\chromedriver.exe");
-		driver = new ChromeDriver();
+		if(nameofbrowser.equals("chrome"))
+		{
+			// Set up WebDriver
+	        System.setProperty("webdriver.chrome.driver", "E:\\chromedriver_win32\\chromedriver.exe");
+			driver = new ChromeDriver();
+		}
+		
+		if(nameofbrowser.equals("edge"))
+		{
+			// Set up WebDriver
+	        System.setProperty("webdriver.edge.driver", "E:\\edgedriver_win32\\msedgedriver.exe");
+			driver = new EdgeDriver();
+		}
+		
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 		driver.manage().window().maximize();
+		Thread.sleep(3000);
 	}
 	
 	@Test(priority=1)
