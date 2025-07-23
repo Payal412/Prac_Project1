cf87d1b30b097f732f249f049a56d5062efbaa20
Payal <P2Test93>
2025-07-23 18:24:23 +0530

    PageObjectModel

--- src/pageobjectmodel/LoginTest.java
+++ src/pageobjectmodel/LoginTest.java
@@ -1,5 +1,7 @@
 package pageobjectmodel;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -7,13 +9,17 @@ import org.testng.annotations.Test;
 public class LoginTest {
 
 	@Test
-	public void loginTest() {
+	public void loginTest() throws Exception {
 		
 		// Set up WebDriver
         System.setProperty("webdriver.chrome.driver", "E:\\chromedriver_win32\\chromedriver.exe");
         
 		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
 		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		
+		Thread.sleep(5000);
 		
 		//Create Object of LoginPage
 		LoginPage login = new LoginPage(driver);
